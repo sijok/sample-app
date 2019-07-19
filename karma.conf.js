2fc5a3dea998908247e9cb0c4ec9f42bf65d8fb4
Sijo Kuriakose <sijo.kuriakose@perfomatix.com>
2019-07-19 12:20:47 +0530

    making config file as an export function

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,18 @@
-exports.config = {
-    reporters: ['junit'],
-    junitReporter: {
-        outputDir: process.env.JUNIT_REPORT_PATH,
-        outputFile: process.env.JUNIT_REPORT_NAME,
-        useBrowserName: false
-    },
+// exports.config = {
+//     reporters: ['junit'],
+//     junitReporter: {
+//         outputDir: process.env.JUNIT_REPORT_PATH,
+//         outputFile: process.env.JUNIT_REPORT_NAME,
+//         useBrowserName: false
+//     },
+// };
+module.exports = function(config) {
+    config.set({
+        reporters: ['junit'],
+        junitReporter: {
+            outputDir: process.env.JUNIT_REPORT_PATH,
+            outputFile: process.env.JUNIT_REPORT_NAME,
+            useBrowserName: false
+        },
+    });
 };

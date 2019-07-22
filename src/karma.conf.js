103d5cdc0993505306a018c0189415e57825c45f
Sijo Kuriakose <sijo.kuriakose@perfomatix.com>
2019-07-22 11:00:06 +0530

    adding karma conf file

---
+++ src/karma.conf.js
@@ -0,0 +1,42 @@
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage-istanbul-reporter'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+        clearContext: false // leave Jasmine Spec Runner output visible in browser
+      },
+      coverageIstanbulReporter: {
+        dir: require('path').join(__dirname, '../coverage'),
+        reports: ['html', 'lcovonly'],
+        fixWebpackSourcePaths: true
+      },
+      reporters: ['spec', 'kjhtml'], // Update progress to spec
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      // updated
+      browsers: ['ChromeHeadless'],
+      // new
+      customLaunchers: {
+        'ChromeHeadless': {
+          base: 'Chrome',
+          flags: [
+            '--no-sandbox',
+            '--headless',
+            '--disable-gpu',
+            '--remote-debugging-port=9222'
+          ]
+        }
+      },
+      singleRun: false,
+      restartOnFileChange: true
+    });
+};

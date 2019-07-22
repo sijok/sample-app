6191362686db86d59fd27727ff859ed501dc2280
Sijo Kuriakose <sijo.kuriakose@perfomatix.com>
2019-07-22 10:58:11 +0530

    jjkk

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,18 @@ module.exports = function (config) {
         require('karma-coverage-istanbul-reporter'),
         require('@angular-devkit/build-angular/plugins/karma')
       ],
+      client: {
+        clearContext: false // leave Jasmine Spec Runner output visible in browser
+      },
+      coverageIstanbulReporter: {
+        dir: require('path').join(__dirname, '../coverage'),
+        reports: ['html', 'lcovonly'],
+        fixWebpackSourcePaths: true
+      },
+      reporters: ['spec', 'kjhtml'], // Update progress to spec
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
       autoWatch: true,
       // updated
       browsers: ['ChromeHeadless'],
